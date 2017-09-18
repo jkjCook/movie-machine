d221cb91e16e3456e235461e5802929748e8de7a
Justin Cook <jkjcook@gmail.com>
2017-09-18 00:04:01 -0400

    testing deploy

--- app/services/getmovie.js
+++ app/services/getmovie.js
@@ -2,12 +2,12 @@ angular.module('myApp').factory('getMovie', ['$http', function ($http) {
 
     var factory = {};
     factory.getMovieById = function (id) {
-        var URL = 'http://mov-api.herokuapp.com/movie/' + id;
+        var URL = 'https://movie-machine.herokuapp.com/movie/' + id;
         return $http.get(URL);
     }
 
     factory.getRandomMovie = function () {
-        var URL = 'http://movie-machine.herokuapp.com/random';
+        var URL = 'https://movie-machine.herokuapp.com/random';
         return $http.get(URL);
     }
 
